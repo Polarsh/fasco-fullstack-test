5b3fed2fbadcbf42a9c8ba4df67fbd9d9ce3fd47
Yorch Sanchez <dybsm98@outlook.es>
2024-12-14 01:26:11 -0500

    se añade fuente roboto

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+import '@fontsource/roboto' // Normal
+import '@fontsource/roboto/700.css' // Negrita
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
