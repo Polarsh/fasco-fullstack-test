ffb3c3a1e6e16981dbb9cc2b924e14f3e4c29e87
Yorch Sanchez <dybsm98@outlook.es>
2024-12-15 14:31:21 -0500

    [ADD] add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/Home'
 import DefaultPage from './pages/Default'
@@ -6,6 +6,10 @@ import Layout from './layout/layout'
 import SignInPage from './pages/SignIn'
 import SignUpPage from './pages/SignUp'
 import ForgotPasswordPage from './pages/ForgotPassword'
+import ShopPage from './pages/Shop'
+import ProductPage from './pages/Product'
+import CartPage from './pages/Cart'
+import CheckoutPage from './pages/Checkout'
 
 export default function App() {
   return (
@@ -18,6 +22,14 @@ export default function App() {
 
       {/* Rutas con el Layout */}
       <Route element={<Layout />}>
+        <Route path='/shop' element={<ShopPage />} />
+        <Route
+          path='/product/'
+          element={<Navigate to='/product/1' replace />}
+        />
+        <Route path='/product/:productId' element={<ProductPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
         <Route path='*' element={<DefaultPage />} />
       </Route>
     </Routes>
