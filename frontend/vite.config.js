fabfd7df2c453fd41569fa22a00c625b6908605b
Yorch Sanchez <dybsm98@outlook.es>
2024-12-15 01:09:49 -0500

    [ADD] levantar servidor local

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permite conexiones externas (0.0.0.0)
+    port: 5173, // Opcional: Forzar el puerto (por defecto es 5173)
+  },
 })
